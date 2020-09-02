4865da445284c227b9a785f6d1325885e9b0ee64
ArslanInfinite <ali.arslan319@gmail.com>
2020-09-02 15:13:21 -0400

    add edit the edit form and ItemController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,17 +10,6 @@ class ItemsController < ApplicationController
     erb :'items/index'
   end 
 
-  get '/items/:id' do 
-    #gets params from url
-    @item = Item.find(params[:id]) #define instance variable for view
-    erb :'items/show' #show single item view
-  end
-
-  get '/items/category/:category' do
-    @items = Item.where(category: params[:category])
-    erb :'items/index'
-  end
-
   get '/items/new' do
     erb :'/items/new'
   end
@@ -35,14 +24,7 @@ class ItemsController < ApplicationController
     end
   end
 
-  post '/add_item_to_pack/:id' do 
-    @item = Item.find(params[:id])
-    @pack = Pack.find(params[:pack_id])
-    PackItem.create(item_id: @item.id, pack_id: @pack.id, user_id: current_user.id)
-    redirect '/items'
-  end 
-
-  get '/items/:id/edit' do 
+  get '/items/:id/edit' do
     #get params from url
     @item = Item.find(params[:id]) #define intstance variable for view
     erb :'items/edit' #show edit item view
@@ -53,7 +35,9 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id]) #define variable to edit
     # Ensure Item can be updated by it's creator
     if current_user.id == @item.id 
-      @item.assign_attributes(params[:item]) #assign new attributes
+      @item.assign_attributes(params[:item])
+      binding.pry 
+      #assign new attributes
       if @item.save #saves new item or returns false if unsuccessful
         redirect '/items' #redirect back to items index page
       else
@@ -63,6 +47,26 @@ class ItemsController < ApplicationController
      redirect '/items'
     end
   end
+
+  get '/items/:id' do 
+    #gets params from url
+    @item = Item.find(params[:id]) #define instance variable for view
+    erb :'items/show' #show single item view
+  end
+
+  get '/items/category/:category' do
+    @items = Item.where(category: params[:category])
+    erb :'items/index'
+  end
+
+  post '/add_item_to_pack/:id' do 
+    @item = Item.find(params[:id])
+    @pack = Pack.find(params[:pack_id])
+    PackItem.create(item_id: @item.id, pack_id: @pack.id, user_id: current_user.id)
+    redirect '/items'
+  end 
+
+  
 
   delete '/items/:id' do
     #get params from url
